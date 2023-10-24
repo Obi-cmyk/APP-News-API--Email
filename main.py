f4985a7fd896f56cc86c15981286dd4628ed6d31
ardit <christophernduike@gmail.com>
2023-10-24 15:26:38 +0100

    An email script

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
+from send_email import send_email
 
 
-api_key = "BIM-BIM"
-
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-09-20&" \
-       "sortBy=publishedAt&apiKey="\
+api_key = "eb7a5794dd8a4b378a1b782372d5222c"
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-09-20&"
+       "sortBy=publishedAt&apiKey="
        "eb7a5794dd8a4b378a1b782372d5222c")
-
+# make a request
 request = requests.get(url)
+
+# Get a dict. with data
 content = request.json()
-for article in content['articles']:
-    length = len(content['articles'])
-    print(article['title'])
-    print(article['description'])
+
+# Access the article titles and descriptions
+body = ""
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = (body + article["title"] + "\n" + article["description"] + "\n"
+                + article[url] + "\n")
+
+body = body.encode("utf-8")
+send_email(message=body)
